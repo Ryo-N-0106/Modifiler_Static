31a14a053bc29b9b10e51f4394ad5ff4151fae54
Ryo-N-0106 <ntkn717416@gmail.com>
2025-05-19 04:42:09 +0700

    Student

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing i
+        Student.change();
+
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
